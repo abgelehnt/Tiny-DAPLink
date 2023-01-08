99f3d2ff7f688a8068129c4b490b84fc4690f8cf
Chi <abgelehnt@foxmail.com>
2023-01-08 10:03:53 +0800

    Init

---
+++ README.md
@@ -0,0 +1,20 @@
+# CH55x_HS_DAP-Link-v2
+基于CH55x低成本USB单片机实现的汇编级优化高速DAP-Link-v2
+
+介绍见：开源CH551/2实现的汇编优化高速DAP-Link (CMSIS-DAP v2)https://whycan.com/t_7786.html
+
+SWD引脚 P1.5和P1.6需并联
+
+SWC引脚 P1.5
+
+串口引脚 P3.0 P3.1
+
+5V下使用应该接1T45电平转换芯片 SWC-DIR P3.4 SWD-DIR P3.2
+
+LED P1.1
+
+复位1.4（接三极管）
+
+拉高RST重新进ISP
+
+使用3.3V供电时需要将两个供电脚短接（CH55xG的15和16脚）一坨锡
---
+++ SRC/Timer.C
@@ -0,0 +1,72 @@
+
+/********************************** (C) COPYRIGHT *******************************
+* File Name          : Timer.C
+* Author             : WCH
+* Version            : V1.0
+* Date               : 2017/01/20
+* Description        : CH554 Time 初始化、定时器、计数器赋值、T2捕捉功能开启函数等
+                       定时器中断函数          		 		   
+*******************************************************************************/
+#include "CH552.H"                                                  
+#include "Debug.H"
+#include "Timer.H" 
+#include "stdio.h"
+
+/*******************************************************************************
+* Function Name  : mTimer_x_ModInit(UINT8 x ,UINT8 mode)
+* Description    : CH554定时计数器x模式设置
+* Input          : UINT8 mode,Timer模式选择
+                   0：模式0，13位定时器，TLn的高3位无效
+                   1：模式1，16位定时器
+                   2：模式2，8位自动重装定时器
+                   3：模式3，两个8位定时器  Timer0
+                   3：模式3，Timer1停止									 
+* Output         : None
+* Return         : 成功  SUCCESS
+                   失败  FAIL
+*******************************************************************************/
+UINT8 mTimer_x_ModInit(UINT8 x ,UINT8 mode)
+{
+    if(x == 0){TMOD = TMOD & 0xf0 | mode;}
+    else if(x == 1){TMOD = TMOD & 0x0f | (mode<<4);}
+    else if(x == 2){RCLK = 0;TCLK = 0;CP_RL2 = 0;}                               //16位自动重载定时器
+    else return FAIL;
+    return SUCCESS;
+}
+
+/*******************************************************************************
+* Function Name  : mTimer_x_SetData(UINT8 x,UINT16 dat)
+* Description    : CH554Timer0 TH0和TL0赋值
+* Input          : UINT16 dat;定时器赋值
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void mTimer_x_SetData(UINT8 x,UINT16 dat)
+{
+    UINT16 tmp;
+    tmp = 65536 - dat;	
+		if(x == 0){TL0 = tmp & 0xff;TH0 = (tmp>>8) & 0xff;}
+		else if(x == 1){TL1 = tmp & 0xff;TH1 = (tmp>>8) & 0xff;}
+		else if(x == 2){
+      RCAP2L = TL2 = tmp & 0xff;                                               //16位自动重载定时器
+      RCAP2H = TH2 = (tmp>>8) & 0xff;
+    }                                                 
+}
+
+void Timer2_Init(void){
+	mTimer2Clk12DivFsys();
+	mTimer_x_ModInit(2,1);
+    mTimer_x_SetData(2,0xF80);
+    mTimer2RunCTL(1);
+    ET2 = 1; 
+}
+
+void mTimer2Interrupt( void ) interrupt INT_NO_TMR2 using 3{                                                                             
+    if(XBUS_AUX & (bUART0_TX | bUART0_RX)){
+		Uart_LED = 0;
+	}else{
+		Uart_LED = 1;
+	}
+	TF2 = 0;
+}
+
